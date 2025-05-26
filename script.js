f08d6db59bd070cba0cb09af66549372d7d48fa3
Anastasia Arefeva <89157321250@mail.ru>
2025-05-26 17:10:15 +0300

    Changed the file index.html

--- script.js
+++ script.js
@@ -1,2 +1,64 @@
-let a = alert("10")
-a = alert("20")
+//Задание 1
+let A = alert("10")
+A = alert("20")
+
+//Задание 2
+let iPhone = prompt("В каком году выпустился первый iPhone?")
+alert(iPhone);
+
+//Задание 3
+let javaScript = prompt("Имя создателя языка JavaScript?")
+alert(javaScript);
+
+//Задание 4
+let sum = 10 + 2;
+alert(sum);
+
+let difference = 10 - 2;
+alert(difference);
+
+let product = 10 * 2;
+alert(product);
+
+let quotient = 10 / 2;
+alert(quotient);
+
+//Задание 5
+let result = 2 ** 5;
+alert(result);
+
+//Задание 6
+let a = 9;
+let b = 2;
+let remainder = a % b;
+alert(remainder);
+
+//Задание 7
+let num = 1;
+num +=5;
+num -=3;
+num *=7;
+num /=3;
+num++;
+num--;
+alert(num);
+
+//Задание 8 
+let age = Number(prompt("Сколько Вам лет?"));
+alert(age);
+
+//Задание 9
+const user = {
+    name: 'Anna',
+    age: 20,
+    isAdmin: true
+}
+
+//Задание 10 
+let Name = prompt("Как вас зовут?");
+alert(`Привет, ${Name}!`);
+
+
+
+
+
