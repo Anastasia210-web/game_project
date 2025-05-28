a2f0bf702665e2cefce3e46f890d1021c7cbf2b0
Anastasia Arefeva <89157321250@mail.ru>
2025-05-29 01:19:09 +0300

    Changed the file script.scss, Changed the file index.scss

--- script.js
+++ script.js
@@ -1,62 +1,141 @@
+//hw-2
 //Задание 1
-let A = alert("10")
-A = alert("20")
+//let A = alert("10")
+//A = alert("20")
 
 //Задание 2
-let iPhone = prompt("В каком году выпустился первый iPhone?")
-alert(iPhone);
+//alert(iPhone);
+//let iPhone = prompt("В каком году выпустился первый iPhone?")
 
 //Задание 3
-let javaScript = prompt("Имя создателя языка JavaScript?")
-alert(javaScript);
+//let javaScript = prompt("Имя создателя языка JavaScript?")
+//alert(javaScript);
 
 //Задание 4
-let sum = 10 + 2;
-alert(sum);
+//let sum = 10 + 2;
+//alert(sum);
 
-let difference = 10 - 2;
-alert(difference);
+//let difference = 10 - 2;
+//alert(difference);
 
-let product = 10 * 2;
-alert(product);
+//let product = 10 * 2;
+//alert(product);
 
-let quotient = 10 / 2;
-alert(quotient);
+//let quotient = 10 / 2;
+//alert(quotient);
 
 //Задание 5
-let result = 2 ** 5;
-alert(result);
+//let result = 2 ** 5;
+//alert(result);
 
 //Задание 6
-let a = 9;
-let b = 2;
-let remainder = a % b;
-alert(remainder);
+//let a = 9;
+//let b = 2;
+//let remainder = a % b;
+//alert(remainder);
 
 //Задание 7
-let num = 1;
-num +=5;
-num -=3;
-num *=7;
-num /=3;
-num++;
-num--;
-alert(num);
+//let num = 1;
+//num +=5;
+//num -=3;
+//num *=7;
+//num /=3;
+//num++;
+//num--;
+//alert(num);
 
 //Задание 8 
-let age = Number(prompt("Сколько Вам лет?"));
-alert(age);
+//let age = Number(prompt("Сколько Вам лет?"));
+//alert(age);
 
 //Задание 9
-const user = {
-    name: 'Anna',
-    age: 20,
-    isAdmin: true
-}
+//const user = {
+    //name: 'Anna',
+    //age: 20,
+    //isAdmin: true
+//}
 
 //Задание 10 
-let Name = prompt("Как вас зовут?");
-alert(`Привет, ${Name}!`);
+//let Name = prompt("Как вас зовут?");
+//alert(`Привет, ${Name}!`);
+
+//hw-3
+//Задание 1 
+let password = '112233';
+let enterPassword = prompt("Введите пароль");
+if (enterPassword === "112233") {
+    alert ("Пароль введен верно")}
+    else {
+        alert ("Пароль введен неправильно")
+    };
+
+//Задание 2
+let c = prompt('Введите число');
+if (c >= 0 && c <= 10) {
+   console.log('Верно')}
+else {console.log('Неверно')};
+
+//Задание 3
+let d = 15;
+let e = 225;
+if (d >= 100 || e >= 100 ) 
+    {console.log('Верно')}
+else {console.log('Неверно')};
+
+//Задание 4
+let a = '2';
+let b = '3';
+alert(+a + +b);
+
+//Задание 5 
+var monthNumber = prompt('Введите число месяца');
+switch (monthNumber) {
+    case '1':
+        alert('Январь')
+        break;
+            case '2':
+        alert('февраль')
+        break;
+                    case '3':
+        alert('Март')
+        break;
+                    case '4':
+        alert('Апрель')
+        break;
+                    case '5':
+        alert('Май')
+        break;
+                    case '6':
+        alert('Июнь')
+        break;
+                    case '7':
+        alert('Июль')
+        break;
+                    case '8':
+        alert('Август')
+        break;
+                    case '9':
+        alert('Сентабрь')
+        break;
+                    case '10':
+        alert('Октябрь')
+        break;
+                    case '11':
+        alert('Ноябрь')
+        break;
+                    case '12':
+        alert('Декабрь')
+        break;
+
+
+    default:
+        alert('Такого месяца нет')
+        break;
+}
+
+
+
+
 
 
 
